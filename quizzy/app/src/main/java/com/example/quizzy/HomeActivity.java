264b8e5a634abba214e982b05da3f4988147cf80
Kodjo <josias.semanou@hei.yncrea.fr>
2020-02-24 12:45:13 +0100

    Changes

--- quizzy/app/src/main/java/com/example/quizzy/HomeActivity.java
+++ quizzy/app/src/main/java/com/example/quizzy/HomeActivity.java
@@ -1,8 +1,15 @@
 package com.example.quizzy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.example.quizzy.adapter.CategoryAdapter;
+import com.example.quizzy.pojo.Category;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +17,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        List<Category> categories = new ArrayList<>();
+        Category category = new Category("e", "e", "e", "e", "e", new ArrayList<>());
+
+        categories.add(category);
+        CategoryAdapter adapter = new CategoryAdapter(categories);
+        RecyclerView rv = findViewById(R.id.home_recycler);
+        rv.setAdapter(adapter);
+
     }
 }
